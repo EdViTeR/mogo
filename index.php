8adbd8e54819d7189a57091389c4708fa2cb5aa1
EdViTeR <maxpivnenko@mail.ru>
2022-03-24 01:57:53 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Mogo</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<div class="header">
+			<div class="header-inner">
+				<div class="logo">Mogo</div>
+					
+				<nav>
+					<a href="#">About</a>
+					<a href="#">Service</a>
+					<a href="#">Blog</a>
+					<a href="#">Work</a>
+					<a href="#">Contact</a>
+				</nav>
+			</div>
+			
+		</div>
+	</header>
+	<div class="intro">
+		<div class="container">
+			<h1>Mogo</h1>
+		</div>
+	</div>
+</body>
+</html>
